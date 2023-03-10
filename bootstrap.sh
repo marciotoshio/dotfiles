1ab8c51ede7e1e04a4b0c485694a31d770fa0c77
Marcio Toshio <marcio@tosh.io>
2023-03-10 10:10:22 -0500

    Fix rvm installation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ function doIt() {
     echo "RVM installed"
   else
       echo "Installing RVM"
-      gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+      gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
       \curl -sSL https://get.rvm.io | bash
   fi;
 
@@ -32,11 +32,6 @@ function doIt() {
     --exclude "LICENSE-MIT.txt" \
     -avh --no-perms . ~;
   echo "Rsynced dot files"
-
-  # Install Sublime Text settings
-  cp -r init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/ 2> /dev/null
-  cp -r init/Markdown.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/ 2> /dev/null
-  echo "Copied Sublime preferences"
 }
 
 if [ "$1" = "--force" -o "$1" = "-f" ]; then
